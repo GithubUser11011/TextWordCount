3a09d05b88121d1283177702385dbdc43ed29d33
GithubUser11011 <102822921+GithubUser11011@users.noreply.github.com>
2022-04-03 11:18:51 -0500

    The stop file is importing properly and I updated the Readme so I wanted to save my work

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # TextWordCount
-Reads in two files and a value N.  The first file are words not to count.  The second file is a block of text.  This project displays the N words with the highest word count in the block of text.
+Reads in two files and a value N.  The first file contains words not to count.  The second file is a block of text.  This project displays the N words with the highest word count in the block of text.
+
+### Design Decisions
+I try to design and code my projects with empathy.  That means considering who will be reading, using, maintaining, upgrading, or fixing bugs in my code later.  To that end, I try to design code that's reusable and easy to reuse.  This extends projects but also lowers technical debt and increases readability.  If it's not easy to use though, people won't use it.  If they are forced to use it, I want it to be as easy to use as possible to avoid frustration.
+
+Coding with empathy also includes making code easier to read.  The brain looks for patterns so simple things like lining up equal signs in blocks of code introduces a pattern that the brain can ignore.  For code reviews, for example, this makes identifying variable names, variable types, what values are being set, and if everything's being set correctly much quicker and easier.  It also means good comments, which I admit I need to get better at, doing things in smaller sections, and not being "clever".  There are sometimes clever, elegant ways to solve a problem however, by definition, you must be twice as clever to debug a piece of code so if you're as clever as you can be while coding, you literally can't debug it.  Also, clever code can be very confusing which is frustrating for others behind you.
+
+To that end, I chose Javascript/HTML/CSS as the language to solve this problem.  I thought about doing a Windows app or C# but Microsoft tends to automatically introduce a lot of complexities when it's trying to be helpful.  That could possibly require extra installations and be frustrating when trying to run it.  Containing everything in 3 simple files should make it easy to download and test.
+
+I also chose to create a class that handled most of the functionality.  This eliminates duplicate coding and creates single failure points that are easier to debug.  It also makes using the functionality in later programs a lot easier.
+
--- scripts.js
+++ scripts.js
@@ -1,14 +1,10 @@
 function initialize()
 {
+   wordCounter = new WordCounter()
+
    // ************************ Drag and drop ***************** //
    stopDropArea = document.getElementById("stop-drop-area")
    textDropArea = document.getElementById("text-drop-area")
-   
-   var stop_file = ""
-   var text_file = ""
-
-   stop_list = []
-   word_map = new Map()
 
    // Prevent default drag behaviors
    ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => 
@@ -73,21 +69,7 @@ function handleStopDrop(e)
    }
    else
    {
-      var file = this.files[0];
-
-      var reader = new FileReader();
-      reader.onload = function(progressEvent)
-      {
-         // Entire file
-         console.log(this.result);
-    
-         // By lines
-         var lines = this.result.split('\n');
-         for(var line = 0; line < lines.length; line++)
-         {
-            console.log(lines[line]);
-         }
-      };
+      wordCounter.setStopFile (files[0])
    }
 }
 
@@ -96,157 +78,71 @@ function handleTextDrop(e)
    var dt = e.dataTransfer;
    var files = dt.files;
 
-   console.log("handleTextDrop");
-
    if (files.length > 1)
    {
       alert ("Please only upload one file at a time")
    }
    else
    {
-      //make sure the file is either a .zip or a .txt file
-      text_file = files[0].name;
-      console.log (text_file)
-
-/*      if (extension == ".xml" || extension == ".txt")
-      {
-         handleFile(txt_file)
-      }
-      else
-      {
-         document.getElementById('error').innerHTML = "Please only upload .txt or .xml files from MTA";
-      }
-   */
+      wordCounter.setTextFile (files[0])
    }
 }
 
-
-function handleFile(txt_file)
+class WordCounter
 {
-   //Provide feedback to the user
-   document.getElementById('error').innerHTML = "Upload successful<br>Please Note: Very large requests/replies will take a while to load but the site is not hanging!";
-
-   var comboBox = document.getElementById('reqs');
-
-   //clear the request list box
-   while (comboBox.options.length > 0)
+   /**
+    * This class takes in a stop file and a text file.
+    * Once both files are set, it reads them in and creates a list of words and how many times they're used in the text file
+    * Get functions are provided to retrieve the list of words and counts
+   */
+   constructor(data)
    {
-      comboBox.removeChild(comboBox.options[0]);
+      this.text_file_set = false;
+      this.stop_file_set = false;
+
+      this.stop_words = new Array();
    }
 
-   var fr = new FileReader(txt_file);
-
-   //Request List has to be populated here because FileReader loads asynch
-   //and this function only runs after FileReader is done loading.
-   //If we move this code outside of this function, it all gets executed 
-   //before FileLoader is ready and everything looks empty
-   //Once it's finished and the request list is populated however,
-   //the rest can be done in the functions handling request list clicks.
-   fr.onload = function ()
+   //Set the stop file - if text file is already set, process everything
+   setStopFile(stop_file)
    {
-      document.getElementById('req_details').textContent = "";
+      //Read in the text file
+      console.log ("In setStopFile")
 
-      var remaining_text = fr.result;
-      var eachLine = [];
+      var fr = new FileReader(stop_file)
+      fr.onload = readSuccess;
 
-      //set file format - xml or old school
-      var xml_pos = remaining_text.search('<RequestMeta>');
-
-      var is_old_school = true;   //determines whether to read old school format or XML
-      if (xml_pos > 0)
+      //This is done after load is successful so load up the stop-list here
+      function readSuccess(evt)
       {
-         is_old_school = false;
-      }
+         const file_text  = evt.target.result
+         var stop_lines = file_text.split(/\r?\n/)
 
-      //Get the position of the next </Request> or </Reply
-      var next_req = 0;
-      var next_rep = 0;
+         //Initialize this.stop_words
+         this.stop_words = []
 
-      var start_block;
-      var end_block;
-
-      var file_complete = 0;
-
-      //variables to skip past <Request> and <Reply> tags for split_pos - declared here so we don't have to search through the code if they need updated
-      var req_spacer = 10;
-      var rep_spacer = 8;
-
-      if (!is_old_school)      //if it's an XML file, we just need to grab everything between the trace lines and create new items with it.
-      {
-         console.log("!old school");
-
-         while (!file_complete)
+         //Remove blank lines and any lines that start with '#' or ' '
+         stop_lines.forEach((line, index, object) =>
          {
-            start_block = 0;
-            end_block = remaining_text.indexOf("[trace-", start_block + 5);
-
-            if (end_block < 0)   //end of file - add whatever's left
+            if (line.length > 0 && line.charAt(0) != '#' && !(line.trim() === ''))
             {
-               file_complete = true;
-               end_block = remaining_text.length;
-            }
-
-            //Create a new item and add it to the list
-            console.log("start = " + start_block + " end = " + end_block);
-            item_list.push(new Item_XML(remaining_text.slice(start_block, end_block)));
-
-            //remove that line from remaining_text
-            remaining_text = remaining_text.slice(end_block, remaining_text.length);
-         }
-      }
-      else
-      {
-         while (!file_complete)
-         {
-            next_req = remaining_text.search('</Request>');
-            next_rep = remaining_text.search('</Reply>');
-
-            if (next_req == -1 && next_rep == -1)
-            {
-               //No more legible data - end of file
-               file_complete = 1;
-               break;
-            }
-            else if (next_req > -1 && next_rep == -1)
-            {
-               //Shouldn't be possible but may be out of sequence - handle anyway
-               split_pos = next_req + req_spacer;
-            }
-            else if (next_req == -1 && next_rep > -1)
-            {
-               //Should be end of file but if sequence is off - handle as if there's more
-               split_pos = next_rep + rep_spacer;
-            }
-            else
-            {
-               //Both still exist in file so get the next one and add appropriate offset
-               if (next_req < next_rep)
+               //while we're adding words, filter out the duplicates
+               if (!this.stop_words.includes(line.trim()))
                {
-                  split_pos = next_req + req_spacer;
-               }
-               else
-               {
-                  split_pos = next_rep + rep_spacer;
+                  this.stop_words.push(line.trim())
                }
             }
-
-            //Create a new item and add it to the list
-            item_list.push(new Item_SRV(remaining_text.slice(0, split_pos)));
-
-            //remove that line from remaining_text
-            remaining_text = remaining_text.slice(split_pos, remaining_text.length);
-         }
+         })
       }
-
-      //Add Request Numbers as options to option list
-      for (i = 0; i < item_list.length; i++)
-      {
-         var option = document.createElement('option');
-         option.value = option.text = item_list[i].getReqNumber();
-         comboBox.appendChild(option);
-      }
-
+      
+      //load the file into memory
+      fr.readAsText(stop_file);
    }
 
-   fr.readAsText(txt_file);
+   //Set the text file - if stop file is already set, process everything
+   setTextFile(text_file_info)
+   {
+      //Read in the text file
+      console.log ("In setTextFile")
+   }
 }
